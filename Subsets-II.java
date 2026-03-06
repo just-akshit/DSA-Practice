767b70067d9919d42ffd726e95d1c71b8f95c64a
Akshit Jain <akshit11jain22@gmail.com>
2026-03-06 16:36:30 +0530

    [90] [Time Beats: 55.81%] [Memory Beats: 97.63%] - LeetPush

--- Subsets-II.java
+++ Subsets-II.java
@@ -1,22 +1,25 @@
 1class Solution {
 2    public List<List<Integer>> subsetsWithDup(int[] nums) {
 3        Arrays.sort(nums);
-4        Set<List<Integer>> ans = new HashSet<>();
+4        List<List<Integer>> ans = new ArrayList<>();
 5        List<Integer> list = new ArrayList<>();
-6        sumWithoutDup(0,nums, ans, list);
+6        sumWithoutDup(0,nums, list, ans);
 7
-8        return new ArrayList<>(ans);
+8        return ans;
 9
 10    }
-11    private void sumWithoutDup(int idx, int[] arr, Set<List<Integer>> ans, List<Integer> list){
-12        int n = arr.length;
-13        if(idx == n){
-14            ans.add(new ArrayList<>(list));
-15            return;
-16        }
-17        list.add(arr[idx]);
-18        sumWithoutDup(idx + 1, arr, ans, list);
-19        list.remove(list.size() - 1);
-20        sumWithoutDup(idx + 1, arr, ans, list);
-21    }
-22}
+11    private void sumWithoutDup(int idx, int[] nums, List<Integer> list, List<List<Integer>> ans ){
+12        int n = nums.length;
+13        ans.add(new ArrayList<>(list));
+14
+15        for(int i = idx; i<n; i++){
+16
+17            if(i != idx && nums[i] == nums[i-1]) continue;
+18
+19            list.add(nums[i]);
+20            sumWithoutDup(i + 1, nums, list, ans);
+21            list.remove(list.size() - 1);
+22        }
+23
+24    }
+25}
