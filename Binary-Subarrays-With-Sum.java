cb50261822ae4f5c894585f0880b3ae70924496d
Akshit Jain <akshit11jain22@gmail.com>
2026-06-20 11:31:49 +0530

    [930] [Time Beats: 6.46%] [Memory Beats: 50.97%] - LeetPush

--- Binary-Subarrays-With-Sum.java
+++ Binary-Subarrays-With-Sum.java
@@ -1,21 +1,19 @@
 1class Solution {
 2    public int numSubarraysWithSum(int[] nums, int goal) {
-3        return number(nums, goal) - number(nums, goal - 1);
-4    }
-5    private int number(int[] nums, int goal){
-6        if(goal < 0) return 0;
-7
-8        int l=0, r=0, cnt=0, sum=0;
-9        int n = nums.length;
-10        while(r<n){
-11            sum += nums[r];
-12            while( sum > goal){
-13                sum -= nums[l];
-14                l++;
-15            }
-16            cnt += (r-l+1);
-17            r++;
-18        }
-19        return cnt;
-20    }
-21}
+3        // HashMap<Integer, Integer> map = new HashMap<>();
+4        // map.put(0,1);
+5        int n = nums.length;
+6        int cnt = 0;
+7        for(int i=0; i<n; i++){
+8            int sum = 0;
+9            for(int j=i; j<n; j++){
+10                sum += nums[j];
+11                if(sum == goal){
+12                    cnt++;
+13                }
+14            }
+15        }
+16        return cnt;
+17
+18    }
+19}
