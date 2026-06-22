f4c01713ff8c3b1caaabbfcef8d2700286ac4a0d
Akshit Jain <akshit11jain22@gmail.com>
2026-06-22 11:32:50 +0530

    [125] [Time Beats: 99.21%] [Memory Beats: 51.83%] - LeetPush

--- Valid-Palindrome.java
+++ Valid-Palindrome.java
@@ -1,19 +1,42 @@
 1class Solution {
 2    public boolean isPalindrome(String s) {
-3        StringBuilder sb = new StringBuilder();
-4        for(char ch : s.toCharArray()){
-5            if(Character.isLetterOrDigit(ch)){
-6                sb.append(Character.toLowerCase(ch));
-7            }
-8        }
-9        int left = 0, right = sb.length() - 1; 
-10        while(left <= right){
-11            if(sb.charAt(left) != sb.charAt(right)){
-12                return false;
-13            }
-14            left++;
-15            right--;
-16        }
-17        return true;
-18    }
-19}
+3        int left = 0;
+4        int right = s.length()-1 ;
+5        while(left < right){
+6            while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
+7                left++;
+8            }
+9            while(left < right && !Character.isLetterOrDigit(s.charAt(right))){
+10                right--;
+11            }
+12            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+13                return false;
+14            }
+15            left++;
+16            right--;
+17        }
+18        return true;
+19    }
+20}
+21
+22
+23// Brute Force = O(n) and O(N)
+24// class Solution {
+25//     public boolean isPalindrome(String s) {
+26//         StringBuilder sb = new StringBuilder();
+27//         for(char ch : s.toCharArray()){
+28//             if(Character.isLetterOrDigit(ch)){
+29//                 sb.append(Character.toLowerCase(ch));
+30//             }
+31//         }
+32//         int left = 0, right = sb.length() - 1; 
+33//         while(left <= right){
+34//             if(sb.charAt(left) != sb.charAt(right)){
+35//                 return false;
+36//             }
+37//             left++;
+38//             right--;
+39//         }
+40//         return true;
+41//     }
+42// }
