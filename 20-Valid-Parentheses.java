fba7d686b079497ae23375d2e9bcde96f20d8d26
Akshit Jain <akshit11jain22@gmail.com>
2026-08-12 16:31:13 +0530

    [20] [Time Beats: 86.05%] [Memory Beats: 15.39%] - LeetPush

--- 20-Valid-Parentheses.java
+++ 20-Valid-Parentheses.java
@@ -1,22 +1,20 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stk = new Stack<>();
+        Deque<Character> stk = new ArrayDeque<>();
         for(char ch : s.toCharArray()){
-            if(ch == '(' || ch == '[' || ch == '{'){
+            if(ch == '(' || ch == '{' || ch == '['){
                 stk.push(ch);
             }
             else{
-                if(stk.isEmpty()){
-                    return false;
-                }
+                if(stk.isEmpty()) return false;
                 char top = stk.pop();
                 if(ch == ')' && top != '('){
                     return false;
                 }
-                if(ch == ']' && top != '['){
+                if(ch == '}' && top != '{'){
                     return false;
                 }
-                if(ch == '}' && top != '{'){
+                if(ch == ']' && top != '['){
                     return false;
                 }
             }
